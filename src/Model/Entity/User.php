671adc4857cb4796c82f655590506f5e466f81aa
Param <parambsingh@gmail.com>
2020-09-12 00:32:14 -0400

    live changes

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity
@@ -78,4 +79,9 @@ class User extends Entity {
     protected $_hidden = [
         'password',
     ];
+    
+     protected function _setPassword($password) {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+    
 }
